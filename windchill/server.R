af6ed2116aad2710cd9b7004c0f22c47cf5307e4
johnbejarano <jbejarano@genomichealth.com>
2015-06-19 14:50:09 -0700

    Update server.R
    Last commit corrected unit conversion bug.  This commit allows 50F to be included in calculation.

--- windchill/server.R
+++ windchill/server.R
@@ -12,7 +12,7 @@ wci <- function(temp, tempscale, windvel, windunits) {
      }
      
      #Calculate wind chill using Imperial units.
-     if(t >= 50) {
+     if(t > 50) {
           windchill <- t
      } else {
           windchill <- (35.74) + (0.6215 * t) - (35.75 * v ^ 0.16) + (0.4275 * t * v ^ 0.16)          
